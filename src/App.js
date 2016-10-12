689999100c86c546c29d86fd930fb0bae6970d5f
Miguel Coronel <m.coronel.seg@gmail.com>
2016-10-12 02:11:53 -0500

    Creates componentes for Listing tasks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import KanbanBoard from './KanbanBoard';
+
 import './App.css';
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+    {
+      id: 1,
+      name: "ContactList Example",
+      done: true
+    },
+    {
+      id: 2,
+      name: "Kanban Example",
+      done: false
+    },
+    {
+      id: 3,
+      name: "My own experiments",
+      done: false
+    }
+    ]
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <KanbanBoard cards={cardsList} />
     );
   }
 }
